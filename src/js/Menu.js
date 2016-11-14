60a0a167106d7f81f0491b40913e445d270eccde
Nuno <e_npgr@yahoo.com>
2016-11-14 17:47:06 -0400

    Some Style on Menu

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router';
+//import { Link } from 'react-router';
+//import { browserHistory } from 'react-router';
 import { observer } from 'mobx-react'
 
 import Drawer from 'material-ui/Drawer';
@@ -12,21 +13,26 @@ import AboutIcon from 'material-ui/svg-icons/action/perm-device-information';
 
 @observer
 export default class Menu extends React.Component {
+	
+	goPage = this.goPage.bind(this)
  
-	About() {
+	goPage(e) {
 		this.props.closeMenu()
-		this.props.history.pushState(null,"about")
-	}
+		let route = e.target.id
+		this.props.history.push(route)
+		//this.context.router.push(route)
+		}
 	render() {
 	 const { closeMenu } = this.props
+	 const { goPage } = this
 	
 	 var barStyle = {backgroundColor: '#3a80ab', fontSize: '16px', fontWeight: '200'}
-	 var ShopIStyle = { color: '68c122'}
-	 var SavedIStyle = { color: 'blue'} //283593
-	 var ProdIStyle = { color: 'EC407A'}
-	 var CustIStyle = { color: 'AB47BC'}
-	 var SetIStyle = { color: 'F57C00'}
-	 var AboutIStyle = { color: '1E88E5'}
+	 var ShopIStyle = { color: '68c122', marginRight: "5px"}
+	 var SavedIStyle = { color: 'blue', marginRight: "5px"}
+	 var ProdIStyle = { color: 'EC407A', marginRight: "5px"}
+	 var CustIStyle = { color: 'AB47BC', marginRight: "5px"}
+	 var SetIStyle = { color: 'F57C00', marginRight: "5px"}
+	 var AboutIStyle = { color: '1E88E5', marginRight: "5px"}
 	 var imgStyle = { width: '200px', height: '95px'}
 	 var menuStyle = {color:'white',marginTop:'-43px',marginLeft:'120px',fontSize:'20px',marginBottom:'27px'}
 	 
@@ -34,32 +40,32 @@ export default class Menu extends React.Component {
 	  <Drawer open={this.props.menuIsOpen} width={200}>
 		<img src="../images/menuimg2.jpg" style={imgStyle}/>
 		<h2 style={menuStyle}>Menu</h2>
-		<div className="menuItem">
+		<div className="menuItem" onClick={goPage} id="order">
 			<ShopIcon style={ShopIStyle}/>
-			<Link to='order' onClick={closeMenu}>New Order</Link>
+			New Order
 		</div>
 		<hr />
-		<div className="menuItem">
+		<div className="menuItem" onClick={goPage} id="saved_orders">
 			<SavedIcon style={SavedIStyle}/>
-			<Link to='saved_orders' onClick={closeMenu}>Saved Orders</Link>
+			Saved Orders
 		</div>
 		<hr />
-		<div className="menuItem">
+		<div className="menuItem" onClick={goPage} id="/">
 			<ProdIcon style={ProdIStyle}/>
-			<Link to='/' onClick={closeMenu}>Products</Link>
+			Products
 		</div>
 		<hr />
-		<div className="menuItem">
-				<CustIcon style={CustIStyle}/>
-			<Link to='customers' onClick={closeMenu}>Customers</Link>
+		<div className="menuItem" onClick={goPage} id="customers">
+			<CustIcon style={CustIStyle}/>
+			Customers
 		</div>
 		<hr />
-		<div className="menuItem">
+		<div className="menuItem" onClick={goPage} id="settings">
 			<SetIcon style={SetIStyle}/>
-			<Link to='settings' onClick={closeMenu}>Settings</Link>
+			Settings
 		</div>
 		<hr />
-		<div className="menuItem" onClick={this.About.bind(this)}>
+		<div className="menuItem" onClick={goPage} id="about">
 			<AboutIcon style={AboutIStyle}/>
 			About
 		</div>
